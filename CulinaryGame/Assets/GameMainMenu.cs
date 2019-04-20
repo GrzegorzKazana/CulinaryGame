0df4682cf1a8b1c297152d479879b69529478721
Rafał Dębski <rafadeb171@student.polsl.pl>
2019-04-20 11:53:25 +0200

    Added some UI logic
    - opening new scene by pressing "Play" button
    - opening options menu by pressing "Options" button,
    - quitting game by pressing "Quit" button,
    - in the options menu there is added functionality which changes the music volume

---
+++ CulinaryGame/Assets/GameMainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        // it is not working in unity debug mode but in real environment it does work
+        Application.Quit();
+    }
+}
---
+++ CulinaryGame/Assets/SetVolume.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+public class SetVolume : MonoBehaviour
+{
+    public AudioMixer _audioMixer;
+
+    public void SetLevel(float sliderValue)
+    {
+        Console.WriteLine("asd");
+        _audioMixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue)*20);
+    }
+}
