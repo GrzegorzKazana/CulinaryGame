70066e4c28d7702c1ed3144b91eaff9b8c708497
Rafał Dębski <rafadeb171@student.polsl.pl>
2019-05-10 00:18:32 +0200

    Code refactoring + dodge delay
    - small code refactoring,
    - test game scene starts after pressing play button
    - added 5 seconds delay time between player's dodges (not fully finished but it's somehow working)

--- CulinaryGame/Assets/TestArea/Scripts/PlayerMovement.cs
+++ CulinaryGame/Assets/TestArea/Scripts/PlayerMovement.cs
@@ -1,72 +1,94 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace Assets.TestArea.Scripts
 {
-
-    private Rigidbody2D PlayerBody;
-    public Rigidbody2D bullet;
-    public float speed = 100;
-    public float jumpModifier = 10;
-    private bool rolling = false;
-    private bool shooting = false;
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerMovement : MonoBehaviour
     {
-        PlayerBody = GetComponent<Rigidbody2D>();
-    }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+        private Rigidbody2D PlayerBody;
+        public Rigidbody2D bullet;
+        public float Speed { get; set; } = 100;
+        public float JumpModifier = 10;
+        private bool _isRolling = false;
+        private bool _isShooting = false;
+
+        private DateTime? _timeWhenWasLastDodge;
+        private readonly double _maxQuantityOfSecondsBetweenDodges = 5;
+        public double TotalMilisecondsToWaitForAnotherDodge;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            rolling = true;
+            PlayerBody = GetComponent<Rigidbody2D>();
         }
 
-        if (Input.GetButtonDown("Fire1"))
+        void Update()
         {
+            if (Input.GetKey(KeyCode.Space) && TotalMilisecondsToWaitForAnotherDodge <= 0)
+            {
+                _isRolling = true;
+            }
 
-            shooting = true;
+            if (Input.GetButton("Fire1"))
+            {
+
+                _isShooting = true;
+            }
+
+            if (_timeWhenWasLastDodge != null)
+            {
+                TotalMilisecondsToWaitForAnotherDodge = (_timeWhenWasLastDodge.Value - DateTime.Now).TotalMilliseconds;
+            }
+
         }
         
-    }
+        void DodgeRoll(Vector2 movement)
+        {
+            
 
-    void dodgeRoll(Vector2 movement)
-    {
-        PlayerBody.AddForce(movement*jumpModifier);
-        rolling = false;
-    }
+            // measure time between last dodges, if it is 1st dodge of the game then
+            // the value is equal to minimum of time to make dodge
+            TotalMilisecondsToWaitForAnotherDodge = _timeWhenWasLastDodge != null
+                ? (_timeWhenWasLastDodge.Value - DateTime.Now).TotalMilliseconds
+                : 0;
 
-    void handleMovement(Vector2 movement)
-    {
-        PlayerBody.AddForce(movement);
-    }
+            // allow user to make dodge only when the time which
+            // passed between 2 dodges is bigger than max quantity of seconds
+            if (TotalMilisecondsToWaitForAnotherDodge <= 0)
+            {
+                PlayerBody.AddForce(movement * JumpModifier);
+                _timeWhenWasLastDodge = DateTime.Now.AddSeconds(_maxQuantityOfSecondsBetweenDodges);
+                _isRolling = false;
+            }
+        }
 
-    void shootBullet(Vector2 movement)
-    {
-        Rigidbody2D bulletInstance = Instantiate(bullet, transform.position, Quaternion.Euler(new Vector2(0, 1))) as Rigidbody2D;
+        void handleMovement(Vector2 movement)
+        {
+            PlayerBody.AddForce(movement);
+        }
 
-
-        shooting = false;
+        void ShootBullet(Vector2 movement)
+        {
+            Rigidbody2D bulletInstance =
+                Instantiate(bullet, transform.position, Quaternion.Euler(new Vector2(0, 1))) as Rigidbody2D;
+            _isShooting = false;
  
-    }
+        }
 
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
 
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+            Vector2 movement = new Vector2(horizontal, vertical);
 
-        Vector2 movement = new Vector2(horizontal, vertical);
+            handleMovement(movement*Speed);
 
-        handleMovement(movement*speed);
-
-        if(rolling) dodgeRoll(movement*speed);
-        if (shooting) shootBullet(movement * speed);
-
-
+            if(_isRolling) DodgeRoll(movement*Speed);
+            if (_isShooting) ShootBullet(movement * Speed);
+        }
     }
 }
